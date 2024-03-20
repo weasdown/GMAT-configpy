d7aba245e45fb27a1051377d36a1ca660017ecfe
William Easdown <34220924+weasdown@users.noreply.github.com>
2024-03-20 10:12:54 +0000

    Created wx_db_copy.py

---
+++ wx_db_copy.py
@@ -0,0 +1,4 @@
+# Test script to copy contents of {GMAT}\depends\wxWidgets\wxWidgets-3.0.4\lib\vc_x64_dll into {GMAT}\application\debug
+# to enable Windows debug build. (See GMT-7534 https://gmat.atlassian.net/browse/GMT-7534)
+
+
