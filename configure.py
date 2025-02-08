e58a783cf1bce5a81b84cbc86b0f1af27fd158ce
weasdown <34220924+weasdown@users.noreply.github.com>
2025-02-08 10:06:41 +0000

    style: invert if for clarity

--- configure.py
+++ configure.py
@@ -564,7 +564,7 @@ def build_swig(plat: str):
 
 def unzip(path_to_zip: str, output_path: str = None) -> None:
     """Unzips a .zip file in a given directory to the current directory, or to a given output directory if specified."""
-    output_arg: str = f'-o "{output_path}"' if output_path is not None else ''
+    output_arg: str = '' if output_path is None else f'-o "{output_path}"'
     os.system(f'"{depends_path}/bin/7za/7za.exe" x {path_to_zip} {output_arg} -r > nul')
 
 
