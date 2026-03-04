2b3061b051c539697f3c3dd7118672d386f2f965
William Easdown Babb <34220924+weasdown@users.noreply.github.com>
2026-03-04 18:40:31 +0000

    feat: add utils.py with various useful functions and classes (some modified from configure.py)

---
+++ utils.py
@@ -0,0 +1,180 @@
+# Utility functions for downloading and extracting archives and running commands.
+
+import os
+from enum import Enum
+from pathlib import Path
+import platform as mac_plat
+import subprocess
+import sys
+import tarfile
+
+
+def run_command(command: str, capture_output: bool = False, check: bool = False, shell: bool = False, stdin: subprocess._FILE = None, stdout: subprocess._FILE = None, stderr: subprocess._FILE = None, text: bool = False, debug: bool = True) -> subprocess.CompletedProcess:
+    """Runs a shell command and returns its `subprocess.CompletedProcess`."""
+    if debug:
+        print(f'Running command "{command}" in "{os.getcwd()}"')
+    process: subprocess.CompletedProcess = subprocess.run(command.split(
+        ' '), capture_output=capture_output, check=check, shell=shell, stdout=stdout, stderr=stderr, text=text)
+    return process
+
+
+class _Directories:
+    def __init__(self) -> None:
+        """Defines useful directories."""
+        _user = run_command('whoami', capture_output=True,
+                            text=True, debug=True).stdout.rstrip()
+
+        self._gmat: Path = Path(f'/home/{_user}/dev/non-OH/gmat/GMAT-R2025a')
+        self._gmat_git: Path = Path(f'/home/{_user}/dev/non-OH/gmat/gmat-git')
+        self._depends: Path = (self._gmat_git/'depends')
+
+    @property
+    def depends(self) -> Path:
+        return self._depends
+
+    @property
+    def gmat(self) -> Path:
+        return self._gmat
+
+    @property
+    def gmat_git(self) -> Path:
+        return self._gmat_git
+
+    def _set_variables(self) -> None:
+        """Sets environment variables for the directories."""
+        os.environ['GMAT_GIT'] = str(self.gmat_git)
+        os.environ['depends'] = str(self.depends)
+        os.environ['GMAT'] = str(self.gmat)
+
+
+directories: _Directories = _Directories()
+
+
+class Platform(Enum):
+    """Defines OS platforms for easier comparison."""
+    Windows = 'win32'
+    macOS = 'macosx'
+    Linux = 'linux'
+
+    @classmethod
+    def current(cls) -> Platform:
+        match sys.platform:
+            case 'win32':
+                return Platform.Windows
+            case 'linux':
+                return Platform.Linux
+            case 'macosx':
+                return Platform.macOS
+            case _:
+                raise RuntimeError(
+                    f'Platform "{sys.platform}" is not recognised.')
+
+
+def cd(directory: Path, debug: bool = False) -> None:
+    """Change directory to `directory`."""
+    os.chdir(str(directory))
+    if debug:
+        print(f'\nSwitched to {directory}\n')
+
+
+def download_file(url: str, save_name: str = '', log: str = '', debug: bool = False) -> int:
+    """Downloads a file from a url and saves it to the current working directory."""
+    if debug:
+        print(
+            f'Downloading from "{url}" and saving as "{os.getcwd()}/{save_name}".')
+    # If no save_name was provided, wget will use the end of the url by default.
+    save_argument: str = '' if not save_name else f'-O {save_name} '
+    return run_command(
+        f'wget -q --show-progress {save_argument}{url}', check=True).returncode
+
+
+def extract(archive: Path, output_path: str = '') -> None:
+    """
+    Unzips a .zip or .tar file in a given directory to the current directory, or to a given output directory if specified.
+
+    .tar files can be .tar, .tar.gz or .tar.bz2.
+
+    :param archive: an archive file to extract.
+    :type archive: str
+
+    :param output_path: an optional path to extract the archive to that if given must be relative to the current working directory. Defaults to the current working directory.
+    :type output_path: str
+
+    :return: None
+    :rtype: NoneType
+    """
+    platform: Platform = Platform.current()
+    extension = archive.suffix
+
+    not_implemented_on_platform = NotImplementedError(
+        f'Extracting of {extension} files on {platform.name} is not yet implemented.')
+
+    if extension == '.zip':
+        if platform == Platform.Windows:
+            seven_zip_exe = f'{directories.depends}/bin/7za/7za.exe'
+            output_path_arg: str = '' if output_path is None else f'-o"./{output_path}" '
+            run_command(
+                f'{seven_zip_exe} x {archive} -r {output_path_arg}> nul')
+        else:
+            raise not_implemented_on_platform
+
+    elif extension == '.bz2':
+        if platform == Platform.Windows:
+            with tarfile.open(archive, 'r:bz2') as tar:
+                tar.extractall(
+                    filter='data', path=output_path if output_path else '.')
+        elif platform == Platform.Linux:
+            run_command(f'tar xjf {archive}')
+
+    elif extension == '.gz':
+        if platform == Platform.Windows:
+            raise not_implemented_on_platform
+        else:  # Linux or macOS
+            run_command(f'tar zxf {archive}')
+
+    else:
+        raise ValueError(f'Archives with the "{extension}" extension cannot be extracted using extract().\n'
+                         f'\nExtraction variables in extract():\n'
+                         f'\t- archive: "{archive}"\n'
+                         f'\t- extension: "{extension}"\n'
+                         f'\t- output_path: "{output_path}"\n')
+
+
+def rm(item: Path, is_directory: bool = False, force: bool = False, debug: bool = False) -> None:
+    """Removes a file or directory."""
+    if debug:
+        print(f'\nRemoving "{item.name}" from "{os.getcwd()}"\n')
+
+    # raise NotImplementedError('rm() function is not yet working.')
+
+    # # FIXME below is not working - files/folders are not deleted.
+    if is_directory:
+        force_arg: str = 'f' if force else ''
+        process = run_command(
+            f'rm -r{force_arg} {str(item.name)}', debug=debug)
+        print(process.stdout)
+    else:
+        force_arg: str = '-f' if force else ''
+        process = run_command(
+            f'rm {force_arg} {str(item.name)}', debug=debug)
+        print(process.stdout)
+
+
+def set_env_variables() -> None:
+    """Sets convenience environment variables for GMAT configuring."""
+    # user = run_command('whoami', capture_output=True,
+    #                    text=True, debug=True).stdout.rstrip()
+    # print(f'user: {user}\n')
+    # set_variables_command = f'{os.environ['GMAT_GIT']}/gmat-install-scripts/set-variables.sh'
+    # process = run_command(set_variables_command,
+    #                       text=True, shell=True, stdout=subprocess.PIPE, debug=True)
+    # print(f'stdout: "{process.stdout}"')
+    # print(f'stderr: "{process.stderr}"')
+    # return process.returncode
+
+    directories._set_variables()
+    print('All environment variables set.')
+
+
+if __name__ == '__main__':
+    pass
