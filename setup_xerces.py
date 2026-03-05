d2263e3d47328edb8a3ebb8780270fab7094d064
weasdown <34220924+weasdown@users.noreply.github.com>
2026-03-05 13:24:15 +0000

    style: organise imports

--- setup_xerces.py
+++ setup_xerces.py
@@ -1,12 +1,13 @@
 # Downloads and compiles the Xerces library that is a GMAT dependency.
 # This should be run from the [GMAT]/depends folder.
 
-from globals import osx_min_version, osx_sdk
 import os
-import utils as u
-from utils import download_file, set_env_variables, Platform
-from pathlib import Path
 import subprocess
+from pathlib import Path
+
+import utils as u
+from globals import osx_min_version, osx_sdk
+from utils import download_file, set_env_variables, Platform
 
 
 def _compile_xerces():
@@ -16,7 +17,7 @@ def _compile_xerces():
     os.makedirs(xerces_install_path, exist_ok=True)
 
     # Set and make path for logs.
-    logs_path = depends/'logs'/'xerces'
+    logs_path = depends / 'logs' / 'xerces'
     os.makedirs(logs_path, exist_ok=True)
 
     # Extra flags needed for macOS.
@@ -50,7 +51,8 @@ def _compile_xerces():
         # Make install Xerces.
         print(f'Make installing {configuration} library...\n')
         subprocess.run(
-            f'make install -j4 > "{logs_path}/xerces_make_install_{configuration}.log" 2>&1', capture_output=True, shell=True)
+            f'make install -j4 > "{logs_path}/xerces_make_install_{configuration}.log" 2>&1', capture_output=True,
+            shell=True)
 
     build_xerces('debug')  # Build debug configuration.
 
@@ -70,7 +72,7 @@ def _download_xerces(version: str) -> Path:
     xerces_url: str = f'https://archive.apache.org/dist/xerces/c/3/sources/xerces-c-{version}.tar.gz'
     download_file(xerces_url)
     archive_name = xerces_url.rsplit('/', 1)[-1]  # 'xerces-c-{version}.tar.gz'
-    return Path(os.getcwd())/archive_name
+    return Path(os.getcwd()) / archive_name
 
 
 def _extract_xerces(archive: Path) -> None:
@@ -86,9 +88,9 @@ if __name__ == '__main__':
     depends: Path = u.directories.depends
 
     # FIXME: make cross-platform
-    xerces_path = depends/'xerces'
-    xerces_build_path = xerces_path/'linux-build'
-    xerces_install_path = xerces_path/'linux-install'
+    xerces_path = depends / 'xerces'
+    xerces_build_path = xerces_path / 'linux-build'
+    xerces_install_path = xerces_path / 'linux-install'
 
     # Switch to depends folder.
     u.cd(depends)
