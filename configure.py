cf7e8ea43289d3d0794b7cbe511864c32078693c
William Easdown Babb <34220924+weasdown@users.noreply.github.com>
2026-03-11 13:26:54 +0000

    refactor: make downloading of PCRE its own function

--- configure.py
+++ configure.py
@@ -159,6 +159,17 @@ def download_depends():
 
         download_complete('CSPICE')
 
+    def download_pcre():
+        # [GMT-6892] Download PCRE into SWIG directory
+        print(f'\nDownloading PCRE {pcre_version} for use with SWIG...')
+        os.chdir(swig_dir)
+        pcre_filename = f'pcre-{pcre_version}.tar.gz'
+        pcre_url: str = f'https://sourceforge.net/projects/pcre/files/pcre/{pcre_version}/{pcre_filename}/download'
+        download_file(pcre_url, pcre_filename)
+        u.rm(swig_dir / pcre_filename)
+
+        download_complete('PCRE')
+
     def download_swig():
         # Download SWIG if it doesn't already exist
         # Check platform-appropriate path
@@ -197,13 +208,7 @@ def download_depends():
 
             download_complete('SWIG')
 
-            # [GMT-6892] Download PCRE into SWIG directory
-            print(f'\nDownloading PCRE {pcre_version} for use with SWIG...')
-            os.chdir(swig_dir)
-            pcre_url: str = f'https://sourceforge.net/projects/pcre/files/pcre/{pcre_version}/{pcre_filename}/download'
-            download_file(pcre_url, pcre_filename)
-
-            download_complete('PCRE')
+            download_pcre()
 
     def download_java():
         # Download Java if it doesn't already exist
@@ -856,7 +861,6 @@ if __name__ == '__main__':
 
     # Set up dir/file names for downloaded files
     cspice_dir = f'cspice{cspice_bit}'
-    pcre_filename = f'pcre-{pcre_version}.tar.gz'
 
     # Get number of cores for multithreaded compilation
     num_cores = str(os.cpu_count())
