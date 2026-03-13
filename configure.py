3ed46822d167e4efe124d62383d61aa59a061267
William Easdown Babb <34220924+weasdown@users.noreply.github.com>
2026-03-13 11:21:35 +0000

    fix: turn off debug for PCRE download

--- configure.py
+++ configure.py
@@ -167,7 +167,7 @@ def download_depends():
         print(f'\nDownloading PCRE {pcre_version} for use with SWIG...')
         os.chdir(swig_dir)
         pcre_url: str = f'https://sourceforge.net/projects/pcre/files/pcre/{pcre_version}/{pcre_filename}/download'
-        download_file(pcre_url, pcre_filename, debug=True)
+        download_file(pcre_url, pcre_filename)
 
         download_complete('PCRE')
 
